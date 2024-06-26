f8481e8c1d121c86ded63f8dae459f5ca57444a6
DenisOluoch <92787522+DenisOluoch@users.noreply.github.com>
2024-06-26 14:07:46 +0300

    Add files via upload

---
+++ Hello.py
@@ -0,0 +1,1 @@
+print("Hello world and welcome to python programing ")
---
+++ collections.py
@@ -0,0 +1,49 @@
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+#Accessing a list by position
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+#Changing the values in a list cherry to orange
+
+myFruitList[2] = "orange"
+print(myFruitList)
+
+#Exercise 2: Introducing the tuple data type
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+#Accessing a tuple by position To access the apple string, enter the following code:
+
+print(myFinalAnswerTuple[0])
+
+#To access the banana string, enter the following code:
+
+print(myFinalAnswerTuple[1])
+#To access the pineapple string, enter the following code:
+
+print(myFinalAnswerTuple[2])
+
+#Exercise 3: Introducing the dictionary data type
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+print(myFavoriteFruitDictionary)
+print(myFavoriteFruitDictionary)
+
+#Accessing a dictionary by name
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
+
+
+
+
+
+
